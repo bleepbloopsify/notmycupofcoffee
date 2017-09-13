554a6b81e2ddf1102804d4fe1a4b763ba6ae8e6a
captiosus <captiosus@users.noreply.github.com>
2017-09-13 13:43:19 -0400

    Fix mvn errors

--- src/main/java/coffee/LegumeLoader.java
+++ src/main/java/coffee/LegumeLoader.java
@@ -1,16 +1,24 @@
 package coffee;
 
-import java.io.File;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.lang.ClassNotFoundException;
+import java.lang.IllegalAccessException;
+import java.lang.InstantiationException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.NoSuchMethodException;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.lang.reflect.Constructor;
+
 
 public class LegumeLoader {
   private String legumefolder;
   private ArrayList<Bean> beans;
 
-  public LegumeLoader(String legumefolder) {
+  public LegumeLoader(String legumefolder) throws ClassNotFoundException, IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
     this.legumefolder = legumefolder;
     File dir = new File(this.legumefolder);
     for (File f : dir.listFiles()) {
@@ -24,14 +32,14 @@ public class LegumeLoader {
     return beans;
   }
 
-  public Bean getBean(String name) {
+  public Bean getBean(String name) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
     Class<?> beanClass = Class.forName(name);
     Constructor<?> constructor = beanClass.getConstructor();
-    Bean bean = constructor.newInstance();
+    Bean bean = (Bean) constructor.newInstance();
     return bean;
   }
 
-  public String sendBean(Bean bean) {
+  public String sendBean(Bean bean) throws IOException {
     final ByteArrayOutputStream baos = new ByteArrayOutputStream();
     final ObjectOutputStream oos = new ObjectOutputStream(baos);
     oos.writeObject(bean);
